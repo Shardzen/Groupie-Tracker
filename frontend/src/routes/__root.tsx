0b34314c94847f197d050201c73be671accee78a
Stormi <xstormi1910@gmail.com>
2026-01-14 20:26:17 +0100

    ajout de la partie player dans le fichiers root

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,6 +1,7 @@
 import { createRootRoute, Outlet } from '@tanstack/react-router'
 import { Navbar } from '../components/layout/navbar'
-import { Footer } from '../components/layout/footer' // <--- Ajout ici
+import { Footer } from '../components/layout/footer'
+import { Player } from '../components/layout/player'
 
 export const Route = createRootRoute({
   component: RootComponent,
@@ -9,46 +10,21 @@ export const Route = createRootRoute({
 function RootComponent() {
   return (
     <>
-      <div className="min-h-screen bg-background text-foreground font-sans antialiased flex flex-col">
+      <div className="min-h-screen bg-background text-foreground font-sans antialiased flex flex-col pb-24">
+        {/* La barre de navigation en haut */}
         <Navbar />
+        
+        {/* Le contenu principal */}
         <main className="flex-1 pt-16">
           <Outlet />
         </main>
-        <Footer /> {/* <--- Ajout ici */}
+
+        {/* Le pied de page */}
+        <Footer />
+
+        {/* Le Lecteur Audio Fixe (Le bijou final) */}
+        <Player />
       </div>
     </>
   )
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
