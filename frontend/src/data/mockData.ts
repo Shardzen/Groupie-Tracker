9c21f037ec03331ddb042a5b80726c1978208000
Lois <ton.email@exemple.com>
2026-01-30 08:42:27 +0100

    feat: ajout de 20 artistes (Lamano, Leto, Weeknd, Metallica...)

--- frontend/src/data/mockData.ts
+++ frontend/src/data/mockData.ts
@@ -35,12 +35,12 @@ export interface Event {
 }
 
 export const mockArtists: Artist[] = [
-  // --- RAP FR (Tes demandes + Classiques) ---
+
   {
     id: '1',
     name: 'Ninho',
     genre: 'Rap FR',
-    image: 'https://e-cdns-images.dzcdn.net/images/artist/f104d4128531e285094269894101e18d/1000x1000-000000-80-0-0.jpg',
+    image:'artists/ninho.jpg.jpg',
     bio: 'Le recordman du rap français. NI est intouchable.',
     topTracks: [
       { title: 'Jefe', plays: '145M', duration: '3:12' },
@@ -52,7 +52,7 @@ export const mockArtists: Artist[] = [
     id: '4',
     name: 'Orelsan',
     genre: 'Rap FR',
-    image: 'https://e-cdns-images.dzcdn.net/images/artist/11833534b86851b4c3ce7d353270912f/1000x1000-000000-80-0-0.jpg',
+    image: 'artists/orelsan.jpg',
     bio: 'Civilisation. La plume la plus aiguisée de Caen.',
     topTracks: [
       { title: 'La Quête', plays: '90M', duration: '4:00' },
@@ -64,7 +64,7 @@ export const mockArtists: Artist[] = [
     id: '7',
     name: 'Leto',
     genre: 'Rap FR',
-    image: 'https://e-cdns-images.dzcdn.net/images/artist/c0356247c41c7b13dd911364d1f27116/1000x1000-000000-80-0-0.jpg',
+    image: 'artists/leto.jpg',
     bio: 'Le roi de la Trap parisienne. Capitaine Jackson.',
     topTracks: [
       { title: 'Macaroni', plays: '80M', duration: '3:20' },
@@ -76,7 +76,7 @@ export const mockArtists: Artist[] = [
     id: '8',
     name: 'Nono La Grinta',
     genre: 'Rap FR',
-    image: 'https://e-cdns-images.dzcdn.net/images/artist/5b6306511a3d905c1926618520295173/1000x1000-000000-80-0-0.jpg',
+    image: 'artists/nonolagrinta.jpg',
     bio: 'La nouvelle pépite du 91. Flow agressif et technique.',
     topTracks: [
       { title: 'Dernier étage', plays: '15M', duration: '2:50' },
@@ -88,7 +88,7 @@ export const mockArtists: Artist[] = [
     id: '9',
     name: 'Keeqaid',
     genre: 'Rap FR',
-    image: 'https://e-cdns-images.dzcdn.net/images/artist/930792120021b38e07886475d4001c9c/1000x1000-000000-80-0-0.jpg',
+    image: 'artists/keeqaid.jpg',
     bio: 'Le futur de la scène. Un style unique qui mélange les genres.',
     topTracks: [
       { title: 'Sensation', plays: '5M', duration: '2:40' },
@@ -100,7 +100,7 @@ export const mockArtists: Artist[] = [
     id: '10',
     name: 'Timar',
     genre: 'Rap FR',
-    image: 'https://e-cdns-images.dzcdn.net/images/artist/77218679589d9a7f394464c8d1976a21/1000x1000-000000-80-0-0.jpg',
+    image: 'artists/timar.jpg',
     bio: 'Une voix qui marque et des textes qui percutent.',
     topTracks: [
       { title: 'Vitesse', plays: '3M', duration: '2:55' },
@@ -112,7 +112,7 @@ export const mockArtists: Artist[] = [
     id: '11',
     name: 'Lamano',
     genre: 'Rap FR',
-    image: 'https://e-cdns-images.dzcdn.net/images/artist/a2c6d48220021350a650d2432a584029/1000x1000-000000-80-0-0.jpg',
+    image: 'artists/lamano.jpg',
     bio: 'L\'étoile montante. À suivre de très près cette année.',
     topTracks: [
       { title: 'Intro', plays: '8M', duration: '2:30' },
@@ -121,12 +121,11 @@ export const mockArtists: Artist[] = [
     upcomingDates: []
   },
 
-  // --- POP ---
   {
     id: '2',
     name: 'Angèle',
     genre: 'Pop',
-    image: 'https://e-cdns-images.dzcdn.net/images/artist/013754d909cb57e500b59b581d0df9eb/1000x1000-000000-80-0-0.jpg',
+    image: '/artists/angele.jpg.webp',
     bio: 'La reine de la pop belge.',
     topTracks: [
       { title: 'Bruxelles je t\'aime', plays: '95M', duration: '3:45' },
@@ -138,7 +137,7 @@ export const mockArtists: Artist[] = [
     id: '12',
     name: 'The Weeknd',
     genre: 'Pop',
-    image: 'https://e-cdns-images.dzcdn.net/images/artist/15869ddfc0413009d17d6b382d56d817/1000x1000-000000-80-0-0.jpg',
+    image: 'artists/theweeknd.jpg',
     bio: 'Starboy. L\'artiste le plus écouté au monde.',
     topTracks: [
       { title: 'Blinding Lights', plays: '3B', duration: '3:20' },
@@ -150,7 +149,7 @@ export const mockArtists: Artist[] = [
     id: '13',
     name: 'Dua Lipa',
     genre: 'Pop',
-    image: 'https://e-cdns-images.dzcdn.net/images/artist/f228e1d556df9c7f68c34114798e404d/1000x1000-000000-80-0-0.jpg',
+    image: 'artists/dualipa.jpg',
     bio: 'La reine du Disco Pop moderne.',
     topTracks: [
       { title: 'Levitating', plays: '1.5B', duration: '3:23' },
@@ -162,7 +161,7 @@ export const mockArtists: Artist[] = [
     id: '14',
     name: 'Stromae',
     genre: 'Pop',
-    image: 'https://e-cdns-images.dzcdn.net/images/artist/43970b55146c965b82e3f5b75f85764d/1000x1000-000000-80-0-0.jpg',
+    image: 'artists/stromae.jpg',
     bio: 'Le maestro belge. Des textes sombres sur des rythmes dansants.',
     topTracks: [
       { title: 'Alors on danse', plays: '900M', duration: '3:26' },
@@ -174,7 +173,7 @@ export const mockArtists: Artist[] = [
     id: '15',
     name: 'Aya Nakamura',
     genre: 'Pop',
-    image: 'https://e-cdns-images.dzcdn.net/images/artist/6b1103b41b12b23dd48467d53b478d65/1000x1000-000000-80-0-0.jpg',
+    image: 'artists/nakamura.jpg',
     bio: 'La reine de France. L\'artiste francophone la plus écoutée à l\'international.',
     topTracks: [
       { title: 'Djadja', plays: '850M', duration: '2:50' },
@@ -183,12 +182,11 @@ export const mockArtists: Artist[] = [
     upcomingDates: []
   },
 
-  // --- METAL ---
   {
     id: '3',
     name: 'Gojira',
     genre: 'Metal',
-    image: 'https://e-cdns-images.dzcdn.net/images/artist/199d35688d01d674b3356bc0c78a0a03/1000x1000-000000-80-0-0.jpg',
+    image: 'artists/gojira.jpg',
     bio: 'La puissance du metal français.',
     topTracks: [
       { title: 'Stranded', plays: '80M', duration: '4:30' },
@@ -200,7 +198,7 @@ export const mockArtists: Artist[] = [
     id: '16',
     name: 'Metallica',
     genre: 'Metal',
-    image: 'https://e-cdns-images.dzcdn.net/images/artist/6e1333e68df6dfb186b5c392b453eb26/1000x1000-000000-80-0-0.jpg',
+    image: 'artists/metallica.jpg',
     bio: 'Les pères fondateurs du Thrash Metal.',
     topTracks: [
       { title: 'Enter Sandman', plays: '1B', duration: '5:31' },
@@ -212,7 +210,7 @@ export const mockArtists: Artist[] = [
     id: '17',
     name: 'Rammstein',
     genre: 'Metal',
-    image: 'https://e-cdns-images.dzcdn.net/images/artist/c31500305a415951c5f3e7924eb3765e/1000x1000-000000-80-0-0.jpg',
+    image: 'artists/rammstein.jpg',
     bio: 'Le metal industriel allemand à son paroxysme. Du feu et de la fureur.',
     topTracks: [
       { title: 'Du Hast', plays: '600M', duration: '3:54' },
@@ -224,7 +222,7 @@ export const mockArtists: Artist[] = [
     id: '18',
     name: 'System of a Down',
     genre: 'Metal',
-    image: 'https://e-cdns-images.dzcdn.net/images/artist/55239df138137357494dfa981297127e/1000x1000-000000-80-0-0.jpg',
+    image: 'artists/systemofadown.jpg',
     bio: 'L\'énergie brute arméno-américaine.',
     topTracks: [
       { title: 'Chop Suey!', plays: '1.2B', duration: '3:30' },
@@ -233,12 +231,11 @@ export const mockArtists: Artist[] = [
     upcomingDates: []
   },
 
-  // --- ELECTRO ---
   {
     id: '5',
     name: 'Daft Punk',
     genre: 'Electro',
-    image: 'https://e-cdns-images.dzcdn.net/images/artist/f2bc007e9133c946ac3c3907ddc509cc/1000x1000-000000-80-0-0.jpg',
+    image: 'artists/daftpunk.jpg',
     bio: 'Les légendes. One More Time.',
     topTracks: [
         { title: 'One More Time', plays: '500M', duration: '5:20' }
@@ -249,7 +246,7 @@ export const mockArtists: Artist[] = [
     id: '19',
     name: 'DJ Snake',
     genre: 'Electro',
-    image: 'https://e-cdns-images.dzcdn.net/images/artist/0b1a03e33b06e9323f494548483b8dd0/1000x1000-000000-80-0-0.jpg',
+    image: 'artists/djsnake.jpg',
     bio: 'Le français qui fait danser la planète entière.',
     topTracks: [
       { title: 'Turn Down for What', plays: '1.1B', duration: '3:33' },
@@ -261,7 +258,7 @@ export const mockArtists: Artist[] = [
     id: '20',
     name: 'David Guetta',
     genre: 'Electro',
-    image: 'https://e-cdns-images.dzcdn.net/images/artist/19e8321a5a0c302920245a468e2f8914/1000x1000-000000-80-0-0.jpg',
+    image: 'artists/davidguetta.jpg',
     bio: 'Le numéro 1 mondial des platines.',
     topTracks: [
       { title: 'Titanium', plays: '1.5B', duration: '4:05' },
@@ -273,7 +270,7 @@ export const mockArtists: Artist[] = [
     id: '21',
     name: 'Kavinsky',
     genre: 'Electro',
-    image: 'https://e-cdns-images.dzcdn.net/images/artist/574241940914a2f8c5b9679237617b0d/1000x1000-000000-80-0-0.jpg',
+    image: 'artists/kavinsky.jpg',
     bio: 'Synthwave legend. Nightcall.',
     topTracks: [
       { title: 'Nightcall', plays: '400M', duration: '4:18' },
@@ -282,12 +279,11 @@ export const mockArtists: Artist[] = [
     upcomingDates: []
   },
   
-  // --- RAP CLOUD ---
   {
     id: '6',
     name: 'PNL',
     genre: 'Rap Cloud',
-    image: 'https://e-cdns-images.dzcdn.net/images/artist/5d2fa7f140a6bdc2c864c3461a9e467e/1000x1000-000000-80-0-0.jpg',
+    image: 'artists/pnl.jpg',
     bio: 'Deux frères. QLF.',
     topTracks: [
         { title: 'Au DD', plays: '300M', duration: '4:00' }
@@ -298,7 +294,7 @@ export const mockArtists: Artist[] = [
     id: '22',
     name: 'Hamza',
     genre: 'Rap Cloud',
-    image: 'https://e-cdns-images.dzcdn.net/images/artist/778673a557b4478345c259838421b4a6/1000x1000-000000-80-0-0.jpg',
+    image: 'artists/hamza.jpg',
     bio: 'Le Sauce God belge. Des mélodies imparables.',
     topTracks: [
       { title: 'Life', plays: '80M', duration: '3:05' },
@@ -310,7 +306,7 @@ export const mockArtists: Artist[] = [
     id: '23',
     name: 'Laylow',
     genre: 'Rap Cloud',
-    image: 'https://e-cdns-images.dzcdn.net/images/artist/41c402120e248f2197607a77d130a84e/1000x1000-000000-80-0-0.jpg',
+    image: 'artists/laylow.jpg',
     bio: 'L\'étrange histoire de Mr. Anderson. Digital et émotionnel.',
     topTracks: [
       { title: 'Megatron', plays: '45M', duration: '3:20' },
